f233fe3d310da035c1bd549c4d0f192fe62feecd
Shadat Rahman <shadat.rahman.464@gmail.com>
2023-10-23 09:20:21 +0600

    init

---
+++ android/app/src/main/kotlin/com/shadatraman/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.shadatraman.dapp_auth_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:dapp_auth_flutter/app/app.dart';
+import 'package:dapp_auth_flutter/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
